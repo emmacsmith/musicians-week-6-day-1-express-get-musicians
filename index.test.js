99b7df1845f008edd036a8d59583a9a42fd2b7b6
emmacsmith <emmacsmith45@hotmail.co.uk>
2024-10-15 10:00:31 +0100

    week 6 day 1 changes

--- index.test.js
+++ index.test.js
@@ -1,5 +1,6 @@
 // install dependencies
 const { execSync } = require('child_process');
+const request = require("supertest")
 execSync('npm install');
 execSync('npm run seed');
 
@@ -12,6 +13,21 @@ const {seedMusician} = require("./seedData");
 
 describe('./musicians endpoint', () => {
     // Write your tests here
+    //send a request to '/musicians' endpoint 
+    test("Testing musicians endpoint", async () => {
+        let response = await request(app).get("/musicians")
+
+        //when sending data from a database the response sends a JSON string. To convert this back to
+        //a JSON object where we can access values, we can use JSON.parse() on the text in the response
+        //write expect tests after
+        let responseData = JSON.parse(response.text)
+
+        //a HTTP status code of 200 indicates a successful GET request was made. 
+        //we can access a status code using the response.statusCode method
+        expect(response.statusCode).toBe(200)
+    })
+    
+   
     
     
 
